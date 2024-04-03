2dd50353713635882b576edf93e7443cbfec3960
hecker-01 <jesseflantua@icloud.com>
2024-04-03 09:53:04 +0200

    `/home` should now work properly

--- src/main/java/net/heckerdev/heckershomes/commands/HomeCommand.java
+++ src/main/java/net/heckerdev/heckershomes/commands/HomeCommand.java
@@ -30,53 +30,50 @@ public class HomeCommand extends BaseCommand{
     @Syntax("(optional) [home]")
     @CommandCompletion("[home] |")
     public void onDefault(@NotNull CommandSender sender, String[] args) {
-        getServer().getScheduler().runTaskAsynchronously(HeckersHomes.getInstance(), () -> {
-            if (!sender.hasPermission("heckershomes.command.home")) {
-                sender.sendMessage(MiniMessage.miniMessage().deserialize("<red>⚠ You do not have permission to use this command!"));
-            } else if (sender instanceof Player) {
-                Player player = (Player) sender;
-                if (this.cooldown.containsKey(player.getUniqueId())) {
-                    if (System.currentTimeMillis() - cooldown.get(player.getUniqueId()) < 3000) {
-                        int secondsLeft = (int) (3 - (System.currentTimeMillis() - cooldown.get(player.getUniqueId())) / 1000);
-                        player.sendMessage(MiniMessage.miniMessage().deserialize("<red><bold>❌</bold> You need to wait " + secondsLeft + " seconds before using this command again!"));
-                    } else {
-                        this.cooldown.put(player.getUniqueId(), System.currentTimeMillis());
-                        executeCommand(player, args);
-                    }
+        if (!sender.hasPermission("heckershomes.command.home")) {
+            sender.sendMessage(MiniMessage.miniMessage().deserialize("<red>⚠ You do not have permission to use this command!"));
+        } else if (sender instanceof Player) {
+            Player player = (Player) sender;
+            if (this.cooldown.containsKey(player.getUniqueId())) {
+                if (System.currentTimeMillis() - cooldown.get(player.getUniqueId()) < 3000) {
+                    int secondsLeft = (int) (3 - (System.currentTimeMillis() - cooldown.get(player.getUniqueId())) / 1000);
+                    player.sendMessage(MiniMessage.miniMessage().deserialize("<red><bold>❌</bold> You need to wait " + secondsLeft + " seconds before using this command again!"));
                 } else {
                     this.cooldown.put(player.getUniqueId(), System.currentTimeMillis());
                     executeCommand(player, args);
                 }
-
             } else {
-                sender.sendMessage(MiniMessage.miniMessage().deserialize("You need be a player to do this!"));
+                this.cooldown.put(player.getUniqueId(), System.currentTimeMillis());
+                executeCommand(player, args);
             }
-        });
+        } else {
+            sender.sendMessage(MiniMessage.miniMessage().deserialize("You need be a player to do this!"));
+        }
     }
 
     private void executeCommand(Player player, String[] args) {
-        getServer().getScheduler().runTask(HeckersHomes.getInstance(), () -> {
-            if (args.length == 0) {
-                if (StorageUtil.doesMainHomeExist(player)) {
-                    StorageUtil.getMainHome(player).thenAccept(location -> {
-                        Bukkit.getLogger().info("Teleporting " + player.getName() + " to their main home at " + location.toString());
-                        player.teleport(location);
-                        player.sendMessage(MiniMessage.miniMessage().deserialize("<green><bold>✔</bold> Successfully sent you to your home!"));
-                    });
-                } else {
-                    player.sendMessage(MiniMessage.miniMessage().deserialize("<red><bold>❌</bold> Your main home doesn't exist yet, create it with /sethome first!"));
-                }
+        if (args.length == 0) {
+            if (StorageUtil.doesMainHomeExist(player)) {
+                StorageUtil.getMainHome(player).thenAccept(location -> {
+                    Bukkit.getLogger().info("Teleporting " + player.getName() + " to their main home at " + location.toString());
+                    player.teleport(location);
+                    Bukkit.getLogger().info("Teleported " + player.getName() + " to their main home at " + location);
+                    player.sendMessage(MiniMessage.miniMessage().deserialize("<green><bold>✔</bold> Successfully sent you to your home!"));
+                });
             } else {
-                if (StorageUtil.doesHomeExist(player, args[0])) {
-                    StorageUtil.getHome(player, args[0]).thenAccept(location -> {
-                        Bukkit.getLogger().info("Teleporting " + player.getName() + " to " + args[0] + " at " + location.toString());
-                        player.teleport(location);
-                        player.sendMessage(MiniMessage.miniMessage().deserialize("<green><bold>✔</bold> Successfully sent you to " + args[0] + "!"));
-                    });
-                } else {
-                    player.sendMessage(MiniMessage.miniMessage().deserialize("<red><bold>❌</bold> That home doesn't exist, create it with /sethome <home> first!"));
-                }
+                player.sendMessage(MiniMessage.miniMessage().deserialize("<red><bold>❌</bold> Your main home doesn't exist yet, create it with /sethome first!"));
             }
-        });
+        } else {
+            if (StorageUtil.doesHomeExist(player, args[0])) {
+                StorageUtil.getHome(player, args[0]).thenAccept(location -> {
+                    Bukkit.getLogger().info("Teleporting " + player.getName() + " to " + args[0] + " at " + location.toString());
+                    player.teleport(location);
+                    Bukkit.getLogger().info("Teleported " + player.getName() + " to " + args[0] + " at " + location);
+                    player.sendMessage(MiniMessage.miniMessage().deserialize("<green><bold>✔</bold> Successfully sent you to " + args[0] + "!"));
+                });
+            } else {
+                player.sendMessage(MiniMessage.miniMessage().deserialize("<red><bold>❌</bold> That home doesn't exist, create it with /sethome <home> first!"));
+            }
+        }
     }
 }
